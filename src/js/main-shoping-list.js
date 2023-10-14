e57f4631ead1bfc87940867201f91ae41f6ba34e
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-10-15 01:22:11 +0300

    fix error and loader

---
+++ src/js/main-shoping-list.js
@@ -0,0 +1,10 @@
+import './authorization/open-authorization';
+document.querySelectorAll('.nav-link').forEach(link => {
+  if (
+    window.location.pathname.endsWith(link.attributes.href.value) ||
+    (window.location.pathname.endsWith('/') &&
+      link.classList.contains('homepage'))
+  ) {
+    link.setAttribute('aria-current', 'page');
+  }
+});
