e5a410f3fae2b94f553c39f743b4e00df0123d88
rmlyaaa <126497199+rmlyaaa@users.noreply.github.com>
2023-10-14 22:25:32 +0000

    Deploying to gh-pages from @ rmlyaaa/project-code-13@18976613f14e2ee5435d5531961677713ad43e86 🚀

---
+++ assets/shoping-7cd230d9.js
@@ -0,0 +1,1 @@
+import"./open-authorization-a1ad6fd8.js";document.querySelectorAll(".nav-link").forEach(t=>{(window.location.pathname.endsWith(t.attributes.href.value)||window.location.pathname.endsWith("/")&&t.classList.contains("homepage"))&&t.setAttribute("aria-current","page")});
