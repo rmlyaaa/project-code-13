200048ffef3c0b3ef09986c82d863c852d780a73
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-10-17 02:31:35 +0300

    1

--- src/main.js
+++ src/main.js
@@ -18,3 +18,13 @@ document.querySelectorAll('.nav-link').forEach(link => {
     link.setAttribute('aria-current', 'page');
   }
 });
+
+document.querySelectorAll('.mob-nav-link').forEach(link => {
+  if (
+    window.location.pathname.endsWith(link.attributes.href.value) ||
+    (window.location.pathname.endsWith('/') &&
+      link.classList.contains('homepage'))
+  ) {
+    link.setAttribute('aria-current', 'page');
+  }
+});
